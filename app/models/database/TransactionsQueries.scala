4c8bd7c546e919f88d7b3e02800185c8d5e0dd30
Lior <lior.liviev@mail.ru>
2019-04-04 10:49:58 +0300

    Code cleanup

--- app/models/database/TransactionsQueries.scala
+++ app/models/database/TransactionsQueries.scala
@@ -6,18 +6,14 @@ import models.{Input, Output, Transaction}
 object TransactionsQueries {
 
   def getTransactionsByBlockId(blockId: String): doobie.ConnectionIO[List[Transaction]] = {
-    val a = sql"SELECT * FROM transactions WHERE blockId = $blockId".query[Transaction].to[List]
-    a
-  }
+    sql"SELECT * FROM transactions WHERE blockId = $blockId".query[Transaction].to[List]
+
 
   def getTransactionInputs(transactionId: String): doobie.ConnectionIO[List[Input]] =
     sql"SELECT * FROM inputs WHERE txId = $transactionId".query[Input].to[List]
 
   def getTransactionOutputs(transactionId: String): doobie.ConnectionIO[List[Output]] = {
-    val a = sql"SELECT * FROM outputs WHERE txId = $transactionId".query[Output].to[List]
-    println(a)
-    a
-  }
+    sql"SELECT * FROM outputs WHERE txId = $transactionId".query[Output].to[List]
+
   def getTransactionById(transactionId: String): doobie.ConnectionIO[Option[Transaction]] =
     sql"SELECT * FROM transactions WHERE id = $transactionId".query[Transaction].to[List].map(_.headOption)
-}
