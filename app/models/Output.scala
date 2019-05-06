cf0a0c972f7a7d915d7b2b0cdd8c32d378c2c9b5
Lior <lior.liviev@mail.ru>
2019-05-06 17:24:04 +0300

    Code cleanup

--- app/models/Output.scala
+++ app/models/Output.scala
@@ -1,15 +1,34 @@
 package models
 
+import io.circe.generic.auto._
 import io.circe.Encoder
+import org.encryfoundation.common.Algos
 import io.circe.syntax._
+import models.Output.Proposition
 
 case class Output(id: String,
-                  boxType: Long,
+                  `type`: Long,
                   txId: String,
-                  monetaryValue: Long,
+                  value: Long,
                   nonce: Long,
-                  coinId: String,
-                  contractHash: String,
+                  tokenId: String,
+                  proposition: Proposition,
                   data: String,
                   isActive: Boolean,
                   minerAddress: String)
+
+object Output{
+
+  case class Proposition(contractHash: String)
+
+  def apply(id: String,
+            `type`: Long,
+            txId: String,
+            value: Long,
+            nonce: Long,
+            tokenId: String,
+            proposition: String,
+            data: String,
+            isActive: Boolean,
+            minerAddress: String): Output = new Output(id, `type`, txId, value, nonce, tokenId, Proposition(proposition), data, isActive, minerAddress)
+}
