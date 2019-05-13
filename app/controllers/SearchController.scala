d8180cdccb490563ebf9658348a8b1b9c866f02e
Lior <lior.liviev@mail.ru>
2019-05-13 15:40:16 +0300

    added opportunity to search blocks and tx

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import models.{Block, FullFilledTransaction, Header, HistoryDao, Input, Output, Transaction, TransactionsDao}
+import models._
 import javax.inject.{Inject, Singleton}
 import play.api.libs.circe.Circe
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
@@ -9,15 +9,27 @@ import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class SearchController @Inject()(cc: ControllerComponents,
-                                       transactionsDao: TransactionsDao,
-                                       historyDao: HistoryDao)
-                                      (implicit ex: ExecutionContext) extends AbstractController(cc)  with Circe {
+                                 transactionsDao: TransactionsDao,
+                                 historyDao: HistoryDao)
+                                (implicit ex: ExecutionContext) extends AbstractController(cc) with Circe {
+
+  def getBlock(id: String): Future[Option[Block]] = {
+    val headerF: Future[Option[Header]] = historyDao.findHeader(id)
+    val payloadF: Future[List[Transaction]] = transactionsDao.transactionsByBlock(id)
+    for {
+      headerOpt <- headerF
+      payload   <- payloadF
+    } yield headerOpt match {
+      case Some(header) => Some(Block(header, payload))
+      case _ => None
+    }
+  }
 
   def getFullTransaction(id: String): Future[Option[FullFilledTransaction]] =
     transactionsDao.transactionById(id).flatMap {
       case Some(tx) =>
         val outputsF: Future[List[Output]] = transactionsDao.outputsByTransaction(tx.id)
-        val inputsF:  Future[List[Input]]  = transactionsDao.inputsByTransaction(tx.id)
+        val inputsF: Future[List[Input]]   = transactionsDao.inputsByTransaction(tx.id)
         for {
           outputs <- outputsF
           inputs  <- inputsF
@@ -25,22 +37,20 @@ class SearchController @Inject()(cc: ControllerComponents,
       case _ => Future(Option.empty[FullFilledTransaction])
     }
 
-  def getBlock(id: String): Future[Option[Block]] = {
-    val headerF: Future[Option[Header]] = historyDao.findHeader(id)
-    val payloadF: Future[List[Transaction]] = transactionsDao.transactionsByBlock(id)
-    for {
-      headerOpt <- headerF
-      payload <- payloadF
-    } yield headerOpt match {
-      case Some(header) => Some(Block(header, payload))
-      case _ => None
-    }
-  }
+  def search(id: String): Action[AnyContent] = Action.async {
 
-  def search(txId: String): Action[AnyContent] = Action.async {
-    getFullTransaction(txId).map {
-      case Some(value) => Ok(views.html.transactionInfo(value))
-      case None => NotFound
+    val blockF: Future[Option[Block]] = getBlock(id)
+    val transactionF: Future[Option[FullFilledTransaction]] = getFullTransaction(id)
+
+    val result: Future[(Option[Block], Option[FullFilledTransaction])] = for {
+      blockOpt       <- blockF
+      transactionOpt <- transactionF
+    } yield (blockOpt, transactionOpt)
+
+    result.map {
+      case (blockOpt, _) if blockOpt.nonEmpty             => Ok(views.html.blockInfo(blockOpt.get))
+      case (_, transactionOpt) if transactionOpt.nonEmpty => Ok(views.html.transactionInfo(transactionOpt.get))
+      case _ => NotFound
     }
   }
 }
