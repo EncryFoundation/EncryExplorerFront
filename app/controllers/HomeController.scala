2cbb5d18afc498203c9846e31a9f4f3cd24413ab
Lior <lior.liviev@mail.ru>
2019-05-16 17:45:31 +0300

    code cleanup

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -9,34 +9,34 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents,
                                historyDao: HistoryDao)
-                              (implicit ex: ExecutionContext) extends AbstractController(cc) with ControllerHelpers with StrictLogging{
+                              (implicit ex: ExecutionContext) extends AbstractController(cc) with ControllerHelpers with StrictLogging {
 
   def index(): Action[AnyContent] = Action.async {
     val headers: Future[List[Header]] = historyDao.lastHeaders()
     val height: Future[Option[Int]] = historyDao.lastHeights()
 
-   val result: Future[(List[Header], Option[Int])] = for {
+    val result: Future[(List[Header], Option[Int])] = for {
       headerOpt <- headers
       heightOpt <- height
-    }yield (headerOpt, heightOpt)
+    } yield (headerOpt, heightOpt)
 
-    result.map{
-      case (headerOpt,heightOpt) => Ok(views.html.index(headerOpt, heightOpt.get))
+    result.map {
+      case (headerOpt, heightOpt) => Ok(views.html.index(headerOpt, heightOpt.getOrElse(0)))
       case _ => NotFound
     }
   }
 
   def listHeadersByHeightRangeApi(to: Int): Action[AnyContent] = Action.async {
-    val range = historyDao.listHeadersByHeightRange(to)
-    val height = historyDao.lastHeights()
+    val range: Future[List[Header]] = historyDao.listHeadersByHeightRange(to)
+    val height: Future[Option[Int]] = historyDao.lastHeights()
 
-    val result = for{
-      rangeOpt <- range
+    val result: Future[(List[Header], Option[Int])] = for {
+      rangeOpt  <- range
       heightOpt <- height
-    }yield (rangeOpt, heightOpt)
+    } yield (rangeOpt, heightOpt)
 
-    result.map{
-      case (rangeOpt,heightOpt) => Ok(views.html.index(rangeOpt, heightOpt.get))
+    result.map {
+      case (rangeOpt, heightOpt) => Ok(views.html.index(rangeOpt, heightOpt.getOrElse(0)))
       case _ => NotFound
     }
   }
