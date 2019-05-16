d5a46b2c4f3ee88bd25aa5333a2314ad17b9921c
Lior <lior.liviev@mail.ru>
2019-05-16 17:33:30 +0300

    next and prev button added to view all the blocks

--- app/models/HistoryDao.scala
+++ app/models/HistoryDao.scala
@@ -9,8 +9,10 @@ class HistoryDao @Inject()(dBService: DBService)(implicit ec: ExecutionContext) 
 
   def lastHeaders(qty: Int = 50): Future[List[Header]] = dBService.runAsync(lastBlocks(qty))
 
+  def lastHeights(): Future[Option[Int]] = dBService.runAsync(lastHeight())
+
   def findHeader(id: String): Future[Option[Header]] = dBService.runAsync(findByIdQuery(id))
 
-  def listHeadersByHeightRange(from: Int): Future[List[Header]] = dBService.runAsync(findByHeightRangeQuery(from))
+  def listHeadersByHeightRange(to: Int): Future[List[Header]] = dBService.runAsync(findByHeightRangeQuery(to))
 
 }
