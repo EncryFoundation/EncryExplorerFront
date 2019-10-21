455de9eba101c84e89e02f88f82067822e96b3f8
capdev <sergey.moskalenko@scruge.world>
2019-10-21 19:03:20 +0500

    transactions pagination web

--- app/controllers/TransactionsController.scala
+++ app/controllers/TransactionsController.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import models.{Contract, FullFilledTransaction, DBInput, Output, TransactionsDao}
+import models.{Contract, DBInput, DBTransaction, FullFilledTransaction, Header, Output, TransactionsDao}
 import javax.inject.{Inject, Singleton}
 import play.api.libs.circe.Circe
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
@@ -20,7 +20,28 @@ class TransactionsController @Inject()(cc: ControllerComponents,
   }
 
   def getUncomTransactions: Action[AnyContent] = Action.async {
-    transactionsDao.unconfirmedTransactions().map(txs => Ok(views.html.unconftransInfo(txs)))
+    transactionsDao.unconfirmedTransactions().map(txs => Ok(views.html.transactions(txs)))
   }
+
+  def indByTxId(txId: String): Int = {
+    0
+  }
+
+  def transactionsPage(txId: String): Action[AnyContent] = Action.async {
+//    val range: Future[List[Header]] = historyDao.listHeadersByHeightRange(to)
+//    val height: Future[Option[Int]] = historyDao.lastHeights()
+//
+//    val result: Future[(List[Header], Option[Int])] = for {
+//      rangeOpt  <- range
+//      heightOpt <- height
+//    } yield (rangeOpt, heightOpt)
+//
+//    result.map {
+//      case (rangeOpt, heightOpt) => Ok(views.html.index(rangeOpt, heightOpt.getOrElse(0)))
+//      case _ => NotFound
+//    }
+    Future(Ok(views.html.transactions(List[DBTransaction]())))
+  }
+
 
 }
