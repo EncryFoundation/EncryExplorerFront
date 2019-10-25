689c009b243cee68c44ca9fb13ce2aacc58c3be3
GRYE <g.yermolaev@encry.ru>
2019-10-25 16:05:09 +0300

    Added list of transaction transfers

--- app/models/Transaction.scala
+++ app/models/Transaction.scala
@@ -7,4 +7,11 @@ case class Transaction(id: String,
                        timestamp: Long,
                        defaultProofOpt: Option[String])
 
-case class FullFilledTransaction(transaction: Transaction, inputs: List[Input], output: List[Output], contract: List[Contract])
+case class FullFilledTransaction(transaction: Transaction, inputs: List[Input], output: List[Output], contract: List[Contract]) {
+
+  val transfers: List[(String, String, String)] = output.foldLeft(Map.empty[(String, String), Long].withDefaultValue(0L)) { case (tr, out) =>
+    if (out.value == 0) tr
+    else tr.updated((out.address, out.tokenId), tr(out.address, out.tokenId) + out.value)
+  }.toList.map { case ((addr, tok), amount) => (addr, tok, (BigDecimal(amount) / 8).toString())}
+
+}
