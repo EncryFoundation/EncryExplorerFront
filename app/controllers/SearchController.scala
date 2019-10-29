9e2c8f19d5b904f226ef3bfaea08b3a12c480caa
GRYE <g.yermolaev@encry.ru>
2019-10-29 13:42:04 +0300

    Fixed search

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -1,20 +1,22 @@
 package controllers
 
+import com.typesafe.scalalogging.StrictLogging
 import models._
 import javax.inject.{Inject, Singleton}
-import org.encryfoundation.common.transaction.{EncryAddress, Pay2ContractHashAddress, Pay2PubKeyAddress, PubKeyLockedContract}
 import play.api.libs.circe.Circe
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Result}
-import scorex.crypto.encode.Base16
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import settings.Utils
+
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
+import scala.util.control.NonFatal
 
 @Singleton
 class SearchController @Inject()(cc: ControllerComponents,
                                  transactionsDao: TransactionsDao,
                                  historyDao: HistoryDao,
                                  boxesDao: BoxesDao)
-                                (implicit ex: ExecutionContext) extends AbstractController(cc) with Circe {
+                                (implicit ex: ExecutionContext) extends AbstractController(cc) with Circe with StrictLogging {
 
   def getBlock(id: String): Future[Option[Block]] = {
     val headerF: Future[Option[Header]] = historyDao.findHeader(id)
@@ -45,12 +47,22 @@ class SearchController @Inject()(cc: ControllerComponents,
 
   def search(id: String): Action[AnyContent] = Action.async {
 
-    val blockF: Future[Option[Block]] = getBlock(id)
+    val blockF: Future[Option[Block]]                       = getBlock(id)
     val transactionF: Future[Option[FullFilledTransaction]] = getFullTransaction(id)
-    val outputF: Future[Option[Output]] = transactionsDao.outputById(id)
-    val walletF: Future[List[Wallet]] = boxesDao.getWalletByHash(Utils.contractHashByAddress(id))
-    val txIdF: Future[List[String]] = boxesDao.getTxsIdByHash(Utils.contractHashByAddress(id))
-    val txsF: Future[List[Transaction]] = txIdF.flatMap(x => Future.sequence(x.map(id => boxesDao.getLastTxById(id))))
+    val outputF: Future[Option[Output]]                     = transactionsDao.outputById(id)
+    val walletF: Future[List[Wallet]]                       = Future
+                                                                .fromTry(Try(Utils.contractHashByAddress(id)))
+                                                                .flatMap(boxesDao.getWalletByHash)
+                                                                .recover {
+                                                                  case NonFatal(_) => List.empty[Wallet]
+                                                                }
+    val txIdF: Future[List[String]]                         = Future
+                                                                .fromTry(Try(Utils.contractHashByAddress(id)))
+                                                                .flatMap(boxesDao.getTxsIdByHash)
+                                                                .recover {
+                                                                  case NonFatal(_) => List.empty[String]
+                                                                }
+    val txsF: Future[List[Transaction]]                     = txIdF.flatMap(x => Future.sequence(x.map(id => boxesDao.getLastTxById(id))))
 
     val result = for {
       blockOpt       <- blockF
@@ -61,10 +73,10 @@ class SearchController @Inject()(cc: ControllerComponents,
     } yield (blockOpt, transactionOpt, outputOpt, walletOpt, txsOpt)
 
     result.map {
-      case (blockOpt, _, _,_,_) if blockOpt.nonEmpty              => Ok(views.html.blockInfo(blockOpt.get))
-      case (_, transactionOpt, _, _,_) if transactionOpt.nonEmpty => Ok(views.html.transactionInfo(transactionOpt.get))
-      case (_,_,outputOpt,_,_) if outputOpt.nonEmpty              => Ok(views.html.outputInfo(outputOpt.get))
-      case (_, _, _, walletOpt,txsOpt) if walletOpt.nonEmpty && txsOpt.nonEmpty         => Ok(views.html.wallet(walletOpt, txsOpt))
+      case (blockOpt, _, _,_,_) if blockOpt.nonEmpty                            => Ok(views.html.blockInfo(blockOpt.get))
+      case (_, transactionOpt, _, _,_) if transactionOpt.nonEmpty               => Ok(views.html.transactionInfo(transactionOpt.get))
+      case (_,_,outputOpt,_,_) if outputOpt.nonEmpty                            => Ok(views.html.outputInfo(outputOpt.get))
+      case (_, _, _, walletOpt,txsOpt) if walletOpt.nonEmpty && txsOpt.nonEmpty => Ok(views.html.wallet(walletOpt, txsOpt))
       case _ => NotFound
     }
   }
