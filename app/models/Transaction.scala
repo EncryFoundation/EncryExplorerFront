73526c2db28da5a0a0d837ee124f106d1ded6ace
GRYE <g.yermolaev@encry.ru>
2019-11-05 11:37:52 +0300

    Transaction view refactoring

--- app/models/Transaction.scala
+++ app/models/Transaction.scala
@@ -1,5 +1,7 @@
 package models
 
+import java.text.DecimalFormat
+
 case class Transaction(id: String,
                        fee: Long,
                        blockId: String,
@@ -8,6 +10,10 @@ case class Transaction(id: String,
                        defaultProofOpt: Option[String])
 
 case class FullFilledTransaction(transaction: Transaction, inputs: List[Input], output: List[Output], contract: List[Contract]) {
+  private val df = new DecimalFormat()
+  df.setMaximumFractionDigits(10)
+
+  val fee: String = df.format(BigDecimal(transaction.fee) / 100000000)
 
   val transfers: List[(String, String, String)] = output.foldLeft(Map.empty[(String, String), Long].withDefaultValue(0L)) { case (tr, out) =>
     if (out.value == 0) tr
